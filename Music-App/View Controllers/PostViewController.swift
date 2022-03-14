affa0389909679389b49f79253b1fdafeef62edc
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-14 13:20:41 -0400

    added view controller for post screen

---
+++ Music-App/View Controllers/PostViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PostViewController.swift
+//  Music-App
+//
+//  Created by Komal Kaur on 3/14/22.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+//MARK: OUTLETS
+    
+
+    
+
+    
+    
+    
+    
+//MARK: VIEWDIDLOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Create a Post"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
