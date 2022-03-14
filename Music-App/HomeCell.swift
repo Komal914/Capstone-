f745c44a7f1fb9f7e4057731ddd0f600d067cdbb
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-14 19:07:51 -0400

    connected table + cell but its not visible-> BUG

---
+++ Music-App/HomeCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeCell.swift
+//  Music-App
+//
+//  Created by Komal Kaur on 3/14/22.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Music-App/View Controllers/HomeViewController.swift
+++ Music-App/View Controllers/HomeViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Home"
+        
+        tableView.delegate = self
+        tableView.delegate = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //should return number of posts
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+        
+        cell.usernameLabel.text = "Komal"
+        
+        return cell
     }
     
 
