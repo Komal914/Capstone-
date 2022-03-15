9011cecbac88737e842596f992ecf07e866a3ae9
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-15 13:38:12 -0400

    added music videos contoller, and music videos cell

---
+++ Music-App/Custom Cells/MusicVideosCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicVideosCell.swift
+//  Music-App
+//
+//  Created by Komal Kaur on 3/15/22.
+//
+
+import UIKit
+
+class MusicVideosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
