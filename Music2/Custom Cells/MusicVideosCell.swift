b4493d402de207b6eb1a013de17943d5776a54e5
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-20 19:34:38 -0400

    vidoes done, bug on buttons

---
+++ Music2/Custom Cells/MusicVideosCell.swift
@@ -0,0 +1,36 @@
+//
+//  MusicVideosCell.swift
+//  Music2
+//
+//  Created by Komal Kaur on 3/20/22.
+//
+
+import UIKit
+
+class MusicVideosCell: UITableViewCell {
+    
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    @IBOutlet weak var albumNameandSongNameLabel: UILabel!
+    
+    
+  
+    @IBOutlet weak var likeButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var commentButton: UIButton!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
