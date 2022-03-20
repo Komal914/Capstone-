fac244387e4f70f85fde81302fa3b1a5671890d7
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-20 19:45:40 -0400

    home screen done, bug on the buttons

---
+++ Music2/Custom Cells/HomeCell.swift
@@ -0,0 +1,53 @@
+//
+//  HomeCell.swift
+//  Music2
+//
+//  Created by Komal Kaur on 3/20/22.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+    
+    @IBOutlet weak var artistImage: UIImageView!
+    
+    
+    @IBOutlet weak var albumNameSongName: UILabel!
+    
+    
+    @IBOutlet weak var albumCover: UIImageView!
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    @IBOutlet weak var captionFromTheUser: UILabel!
+    
+    
+    @IBAction func likeButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func commentButton(_ sender: Any) {
+    }
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Music2/View Controllers/HomeViewController.swift
+++ Music2/View Controllers/HomeViewController.swift
@@ -7,12 +7,52 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+
+    
+    @IBOutlet weak var table: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Home"
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        print("Home screen")
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+   
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //should return number of posts
+        return 1
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 550 //or whatever you need
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        let cell = table.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+        
+      
+        
+        return cell
     }
     
 
