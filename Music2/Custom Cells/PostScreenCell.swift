474d228361432a59526085eeca8169908fb49938
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-04-22 15:29:17 -0400

    moved image, artis name + song info into the cell

---
+++ Music2/Custom Cells/PostScreenCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostScreenCell.swift
+//  Music2
+//
+//  Created by Komal Kaur on 4/22/22.
+//
+
+import UIKit
+
+class PostScreenCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
