437c79f5dc2dbf42734cf44e614f730c383e5a04
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-05-12 14:36:28 -0400

    accessed data

--- Music2/View Controllers/Music Videos/MusicVideosViewController.swift
+++ Music2/View Controllers/Music Videos/MusicVideosViewController.swift
@@ -11,7 +11,7 @@ import MediaPlayer
 import AVFoundation
 
 
-class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var aboutToBecomeInvisibleCell = -1
     var avPlayerLayer: AVPlayerLayer!
@@ -19,12 +19,15 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
     var firstLoad = true
     var visibleIP : IndexPath?
     var currentVideo = String()
+    var searchText = " "
     
-    var videoData = [[String: Any?]]() //array of dictionaries
+    var videoData = NSDictionary()
     var filteredVideoData = [[String: Any?]]()
     
 //MARK: OUTLETS
     @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     @IBAction func commentsButton(_ sender: Any) {
         performSegue(withIdentifier: "musicCommentSegue", sender: self)
@@ -38,6 +41,7 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
         
         table.delegate = self
         table.dataSource = self
+        searchBar.delegate = self
         visibleIP = IndexPath.init(row: 0, section: 0)
         
     //MARK: API REQUEST
@@ -65,7 +69,24 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
         
 //MARK: API REQUEST
         
-        let url = URL(string:"https://api.music.apple.com/v1/catalog/us/music-videos?ids=1553279848,1549013065")!
+        let terms = ["billie", "pop", "doja", "selena", "bad"]
+        
+        func random(terms: [String]) -> String {
+            return terms[Int(arc4random_uniform(UInt32(terms.count)))]
+        }
+        
+        let term = random(terms: terms)
+        print("MYYYYYYY TERM", term )
+        
+        let replaced = term
+        let base = "https://api.music.apple.com/v1/catalog/us/search?types=music-videos&term="
+        let final = base + replaced
+
+        
+        
+//        let url = URL(string:"https://api.music.apple.com/v1/catalog/us/music-videos?ids=1553279848,1549013065")!
+        
+        let url = URL(string: final)!
 
         var request = URLRequest(url: url)
         request.setValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
@@ -78,7 +99,16 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
 
             do {
                 let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-               self.videoData = json?["data"] as! [[String: Any]]
+                print("MY DATA")
+               // print(json)
+                let data = json?["results"] as! NSDictionary
+                let musicVideos = data["music-videos"] as! NSDictionary
+                print(musicVideos)
+                
+                
+                self.videoData = (json?["results"] as! NSDictionary)
+                //print("videoData")
+                //print(self.videoData)
                 DispatchQueue.main.async {
                     self.table.reloadData()
                 }
@@ -122,22 +152,24 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
         let cell = table.dequeueReusableCell(withIdentifier: "MusicVideosCell") as! MusicVideosCell
         
                 
-        let video = videoData[indexPath.row] as NSDictionary        //each video data
-        let attributes = video["attributes"] as! NSDictionary       //attributes for each video
-        let name = attributes["name"] as! String
-        //currentVideo = name
-        let previews = attributes["previews"] as! NSArray
-        let artwork = previews[0] as! NSDictionary
-        let musicVideoUrl = artwork["url"] as! String
-        let artistName = attributes["artistName"] as! String
-        cell.artistNameLabel!.text = artistName
-        cell.albumNameandSongNameLabel!.text = name
-        let videoURL = URL(string: musicVideoUrl)                   //turn string into URL
+        //let video = videoData[indexPath.row] as NSDictionary        //each video data
         
-        self.videoURLs.append(videoURL!)
         
-        //print(videoURLs)
-        cell.videoPlayerItem = AVPlayerItem.init(url: videoURLs[indexPath.row % 2])
+//        let attributes = video["attributes"] as! NSDictionary       //attributes for each video
+//        let name = attributes["name"] as! String
+//        //currentVideo = name
+//        let previews = attributes["previews"] as! NSArray
+//        let artwork = previews[0] as! NSDictionary
+//        let musicVideoUrl = artwork["url"] as! String
+//        let artistName = attributes["artistName"] as! String
+//        cell.artistNameLabel!.text = artistName
+//        cell.albumNameandSongNameLabel!.text = name
+//        let videoURL = URL(string: musicVideoUrl)                   //turn string into URL
+//
+//        self.videoURLs.append(videoURL!)
+//
+//        //print(videoURLs)
+//        cell.videoPlayerItem = AVPlayerItem.init(url: videoURLs[indexPath.row % 2])
         return cell
     }
     
@@ -217,6 +249,73 @@ class MusicVideosViewController: UIViewController, UITableViewDelegate, UITableV
             videoCell.stopPlayback()
         }
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchText = searchBar.text! //user input
+        print(searchText)
+        
+//        //MARK: Replace api call
+//        let replaced = searchText.replacingOccurrences(of: " ", with: "+" )
+//        let base = "https://api.music.apple.com/v1/catalog/us/search?types=songs&term="
+//        let final = base + replaced
+//
+//        let developerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjQ1OVlDU0NWN04ifQ.eyJpc3MiOiI0VlMzWEFQWFRWIiwiZXhwIjoxNjYzNTcyNzMzLCJpYXQiOjE2NDc4MDQ3MzN9.J-jb_NnC82o7oSlFvLt84mf7AkNJ3o8Fhhld4ADIDmgY6NfUBVprpD7y1yqX3pjtIUFI85RDxE2yKS12TFmVuA"
+//
+//
+//        let url = URL(string: final)!
+//
+//        var request = URLRequest(url: url)
+//        request.setValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
+//
+//        let session = URLSession.shared
+//        let task = session.dataTask(with: request) {(data, response, error) in
+//        guard let data = data else {
+//                return
+//        }
+//
+//        do {
+//            let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+//            self.songData = json?["results"] as! NSDictionary
+//            let songs = self.songData["songs"] as! NSDictionary
+//            let numberOfSongs = songs["data"] as! NSArray
+//
+////MARK: ONE SONG DATA
+//            let firstSong = numberOfSongs[0] as! NSDictionary
+//            // print(firstSong) api data for one song
+//            let attributes = firstSong["attributes"] as! NSDictionary
+//            let artWork = attributes["artwork"] as! NSDictionary
+//            let name = attributes["name"] as! String
+//            let artistName = attributes["artistName"] as! String
+//
+//            self.songMenu.append(name)
+//
+////MARK: DROPDOWN DATASOURCE
+//            self.dropDown.dataSource = self.songMenu
+//            let albumName = attributes["albumName"] as! String
+//            let songInfo = albumName + " - " + name
+//            let previews = attributes["previews"] as! NSArray  //music audio
+//            //print("Music Audio: ",previews)
+//            let audioDic = previews[0] as! NSDictionary //going inside the music preview array
+//            print(audioDic)
+//            let musicUrl = audioDic["url"] as! String
+//            //print(musicUrl)
+//            self.audios.append(musicUrl)
+//
+//            let songLabel = songInfo
+//            let genres = attributes["genreNames"] as! NSArray
+//            self.genre = genres
+//
+//            let genreInfo = self.genre[0] as! String
+//
+//            DispatchQueue.main.async {
+//                //STORE THIS
+//                self.Genre = genreInfo
+//                self.ArtistName = artistName
+////              self.genresLabel.backgroundColor = random(colors: myColors)
+////              self.genresLabel.layer.masksToBounds = true
+////              self.genresLabel.layer.cornerRadius = 8
+//                self.table.reloadData()
+            }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
