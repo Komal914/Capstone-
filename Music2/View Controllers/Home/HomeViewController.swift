ddf7cb91b3084355bbd27be3df7b0c6396b810f9
Amogh Kalyan <80735346+amoghub@users.noreply.github.com>
2022-05-16 22:55:25 -0400

    recommendations work based on user genres

--- Music2/View Controllers/Home/HomeViewController.swift
+++ Music2/View Controllers/Home/HomeViewController.swift
@@ -15,14 +15,19 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     var user: User?
     var posts = [PFObject]()
     var username = String()
-    var likedGenres = [String]()
-    var randomGenre = String()
     var aboutToBecomeInvisibleCell = -1
     var visibleIP : IndexPath?
     var currentPostUsername = String()
     var currentSong = String()
-    var recommendData = NSDictionary()
-        
+    
+    // for recommendation cell
+    var likedGenres = [String]()
+    var randomGenre = String()
+    var recommendedSong = String()
+    var recommendedAlbum = String()
+    var recommendedArtist = String()
+    var recommendedAlbumURL = String()
+    var recommendedPreview = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,80 +58,67 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             if genres != nil {
                 let temp = genres![0]
                 self.likedGenres = temp["genre"] as! [String]
-                print(self.likedGenres)
                 self.randomGenre = self.likedGenres.randomElement()!
-                print("oh my lord",self.randomGenre)
                 
-                /*let developerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjQ1OVlDU0NWN04ifQ.eyJpc3MiOiI0VlMzWEFQWFRWIiwiZXhwIjoxNjYzNTcyNzMzLCJpYXQiOjE2NDc4MDQ3MzN9.J-jb_NnC82o7oSlFvLt84mf7AkNJ3o8Fhhld4ADIDmgY6NfUBVprpD7y1yqX3pjtIUFI85RDxE2yKS12TFmVuA"
+                let developerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjQ1OVlDU0NWN04ifQ.eyJpc3MiOiI0VlMzWEFQWFRWIiwiZXhwIjoxNjYzNTcyNzMzLCJpYXQiOjE2NDc4MDQ3MzN9.J-jb_NnC82o7oSlFvLt84mf7AkNJ3o8Fhhld4ADIDmgY6NfUBVprpD7y1yqX3pjtIUFI85RDxE2yKS12TFmVuA"
                 
-              let base = "https://api.music.apple.com/v1/catalog/us/search?types=songs&term="
-                let end = self.randomGenre
+                // get data from random genre
+                let base = "https://api.music.apple.com/v1/catalog/us/search?types=songs&term="
+                let end = self.randomGenre.replacingOccurrences(of: " ", with: "+")
                 let final = base + end
                 let url = URL(string:final)!
-                        var request = URLRequest(url: url)
-                        request.setValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
-                        let session = URLSession.shared
-                        //print("Starting task")
-                        let task = session.dataTask(with: request) { data, response, error in
-                            guard let data = data else {
-                                return
-                            }
-                            
-                            do {
-                                let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                                let results = json!["results"] as! NSDictionary
-                                //print(results)
-                                let songs = results["songs"] as! NSDictionary
-                                let data = songs["data"] as! NSArray
-                                let attribute = data[0] as! NSDictionary
-                                let songInfo = attribute["attributes"] as! NSDictionary
-                                self.recommendData = songInfo
-                                let albumName = songInfo["albumName"] as! String
-                                //print(albumName)
-                            }
-                            
-                            catch {
-                                
-                            }
-                        }
-                        task.resume()*/
+                
+                var request = URLRequest(url: url)
+                request.setValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
+                let session = URLSession.shared
+                
+                let task = session.dataTask(with: request) { data, response, error in
+                    guard let data = data
+                    
+                    else {
+                        return
+                    }
+                           
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                        let results = json!["results"] as! NSDictionary
+                        
+                        let songs = results["songs"] as! NSDictionary
+                        let data = songs["data"] as! NSArray
+                        let attribute = data[0] as! NSDictionary
+                        let songInfo = attribute["attributes"] as! NSDictionary
+                        print(songInfo)
+                        let album = songInfo["albumName"] as! String
+                        let artist = songInfo["artistName"] as! String
+                        
+                        let song = songInfo["name"] as! String
+                        let artwork = songInfo["artwork"] as! NSDictionary
+                        let tempURL = artwork["url"] as! String
+                        let albumURL = tempURL.replacingOccurrences(of: "{w}x{h}", with: "329x329")
+                        
+                        let preview = songInfo["previews"] as! NSArray
+                        print(preview)
+                        let tempPreview = preview[0] as! NSDictionary
+                        let musicPreview = tempPreview["url"] as! String
+                        print(musicPreview)
+                        
+                        
+                        self.recommendedAlbumURL = albumURL
+                        self.recommendedAlbum = album
+                        self.recommendedArtist = artist
+                        self.recommendedSong = song
+                        self.recommendedPreview = musicPreview
+                        
+                        
+                    }
+                    
+                    catch {
+                        
+                    }
+                }
+                task.resume()
             }
         }
-        
-        //print("oh my",self.randomGenre)
-        
-//        let developerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjQ1OVlDU0NWN04ifQ.eyJpc3MiOiI0VlMzWEFQWFRWIiwiZXhwIjoxNjYzNTcyNzMzLCJpYXQiOjE2NDc4MDQ3MzN9.J-jb_NnC82o7oSlFvLt84mf7AkNJ3o8Fhhld4ADIDmgY6NfUBVprpD7y1yqX3pjtIUFI85RDxE2yKS12TFmVuA"
-//
-//        print("load ", self.likedGenres)
-//        let url = URL(string:"https://api.music.apple.com/v1/catalog/us/search?types=songs&term=Hip+Hop")!
-//                var request = URLRequest(url: url)
-//                request.setValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
-//                let session = URLSession.shared
-//                //print("Starting task")
-//                let task = session.dataTask(with: request) { data, response, error in
-//                    guard let data = data else {
-//                        return
-//                    }
-//
-//                    do {
-//                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-//                        let results = json!["results"] as! NSDictionary
-//                        print(results)
-//                        let songs = results["songs"] as! NSDictionary
-//                        let data = songs["data"] as! NSArray
-//                        let attribute = data[0] as! NSDictionary
-//                        let songInfo = attribute["attributes"] as! NSDictionary
-//                        let albumName = songInfo["albumName"] as! String
-//                        print(albumName)
-//                    }
-//
-//                    catch {
-//
-//                    }
-//                }
-//                task.resume()
-        
-        //createRandomGenre()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -148,15 +140,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                 print("error quering for posts: \(String(describing: error))")
             }
         }
-        print("I AM HERE")
-        
-       
-        
-    
-    
-        
-        
     }
+    
     /*
     // fetches random genre
     func createRandomGenre() {
@@ -182,15 +167,18 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("HAIIIIII",self.recommendData)
         let indexPaths = self.table.indexPathsForVisibleRows
         var cells = [Any]()
         for ip in indexPaths!{
             if let HomeCell = self.table.cellForRow(at: ip) as? HomeCell{
                 cells.append(HomeCell)
             }
+            
+            if let recCell = self.table.cellForRow(at: ip) as? RecommenderCell {
+                cells.append(recCell)
+            }
         }
-    
+        
         let cellCount = cells.count
         
         if cellCount == 0 {
@@ -198,18 +186,19 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
         
         if cellCount == 1 {
-            if visibleIP != indexPaths?[0]{
+            if visibleIP != indexPaths?[0] {
                 visibleIP = indexPaths?[0]
             }
             
             if let homeCell = cells.last! as? HomeCell{
+                print(cells.last!)
                 currentPostUsername = self.getUserName(cell: homeCell, indexPath: (indexPaths?.last)!)
                 currentSong = self.getcurrentSong(cell: homeCell, indexPath: (indexPaths?.last)!)
 
                 //print("POST 0",currentPostUsername)
                 //print("POST 0", currentSong)
-
             }
+            
         }
     
         if cellCount >= 2 {
@@ -219,7 +208,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 
                 // curerntHeight is the height of the cell that is visible
                 let currentHeight = intersect.height
-                //print("\n \(currentHeight)")
                 let cellHeight = (cells[i] as AnyObject).frame.size.height
                 
                 // 0.95 here denotes how much you want the cell to display for it to mark itself as visible, .95 denotes 95 percent, you can change the values accordingly
@@ -240,63 +228,83 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    func getcurrentSong(cell: HomeCell, indexPath: IndexPath) -> String{
+    func getcurrentSong(cell: HomeCell, indexPath: IndexPath) -> String {
         return cell.albumNameSongName.text!
     }
     
-    func getUserName(cell : HomeCell, indexPath : IndexPath) -> String{
+    func getUserName(cell : HomeCell, indexPath : IndexPath) -> String {
         return cell.usernameButton.currentTitle!
     }
     
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 672          //or whatever you need
+        return 550          //or whatever you need
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      
-        let cell = table.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
-        let recommenderCell = table.dequeueReusableCell(withIdentifier: "RecommenderCell") as! RecommenderCell
         
-        var reversePosts = [PFObject]()
-        reversePosts = posts.reversed()
-        
-        let post = reversePosts[indexPath.row]
-        
-        // songinfo
-        cell.albumNameSongName.text = post["song"] as? String
-        
-        // song cover
-        let imageFile = post["cover"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        cell.albumCover.af.setImage(withURL: url)
-        
-        // caption
-        let caption = post["caption"] as! String
-        cell.captionFromTheUser.text = caption
-        
-        // artist name
-        cell.artistNameLabel.text = post["artistName"] as? String
-        
-        // genres
-        cell.genreLabel.text = post["genre"] as? String
-        cell.genreLabel.layer.masksToBounds = true
-        cell.genreLabel.layer.cornerRadius = 8
-        
-        // sound file
-        let sound = post["audio"] as! String
-        let soundUrl = URL(string: sound)
-        //sendng music url to cell class
-        cell.videoPlayerItem = AVPlayerItem.init(url: soundUrl!)
-        
-        // username
-        if (post["username"] != nil){
-            let userName = post["username"] as! String
-            cell.usernameButton.setTitle(userName, for: .normal)
+        // recommended cell
+        if indexPath.row == posts.count / 2 {
+            let recommenderCell = table.dequeueReusableCell(withIdentifier: "RecommenderCell") as! RecommenderCell
+            
+            // songInfo
+            recommenderCell.songLabel.text = recommendedSong
+            recommenderCell.artistLabel.text = recommendedArtist
+            
+            // image
+            let recommenderUrl = URL(string: recommendedAlbumURL)!
+            recommenderCell.albumView.af.setImage(withURL: recommenderUrl)
+            
+            // player
+            let soundURL = URL(string: recommendedPreview)
+            recommenderCell.videoPlayerItem = AVPlayerItem.init(url: soundURL!)
+            
+            return recommenderCell
         }
         
-        return cell
+        // regular post
+        else {
+            let cell = table.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+            
+            var reversePosts = [PFObject]()
+            reversePosts = posts.reversed()
+            
+            let post = reversePosts[indexPath.row]
+            
+            // songinfo
+            cell.albumNameSongName.text = post["song"] as? String
+            
+            // song cover
+            let imageFile = post["cover"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.albumCover.af.setImage(withURL: url)
+            
+            // caption
+            let caption = post["caption"] as! String
+            cell.captionFromTheUser.text = caption
+            
+            // artist name
+            cell.artistNameLabel.text = post["artistName"] as? String
+            
+            // genres
+            cell.genreLabel.text = post["genre"] as? String
+            cell.genreLabel.layer.masksToBounds = true
+            cell.genreLabel.layer.cornerRadius = 8
+            
+            // sound file
+            let sound = post["audio"] as! String
+            let soundUrl = URL(string: sound)
+            //sendng music url to cell class
+            cell.videoPlayerItem = AVPlayerItem.init(url: soundUrl!)
+            
+            // username
+            if (post["username"] != nil){
+                let userName = post["username"] as! String
+                cell.usernameButton.setTitle(userName, for: .normal)
+            }
+            
+            return cell
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- Music2/View Controllers/Home/cells/RecommenderCell.swift
+++ Music2/View Controllers/Home/cells/RecommenderCell.swift
@@ -6,16 +6,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecommenderCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setUpPlayer()
     }
     
-    
+    var avPlayer: AVPlayer?
     @IBOutlet var songLabel: UILabel!
-    //self.songLabel.text = "RECOMMENDER"
+    @IBOutlet var albumView: UIImageView!
+    @IBOutlet var artistLabel: UILabel!
     
+    var videoPlayerItem: AVPlayerItem? = nil {
+        didSet {
+            /*
+             If needed, configure player item here before associating it with a player.
+             (example: adding outputs, setting text style rules, selecting media options)
+             */
+            avPlayer?.replaceCurrentItem(with: self.videoPlayerItem)
+        }
+    }
+     
+     func setUpPlayer(){
+         self.avPlayer = AVPlayer.init(playerItem: self.videoPlayerItem)
+                        avPlayer?.volume = 3
+         avPlayer?.actionAtItemEnd = .none
+     }
+    
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        stopPlayback()
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        startPlayback()
+    }
+    
+    func stopPlayback(){
+        self.avPlayer?.pause()
+    }
+
+    func startPlayback(){
+        self.avPlayer?.play()
+    }
 }
